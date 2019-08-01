a4f321a12438ca65928677a6c7890406b21e6692
DESKTOP-5TBM399\lx <1666972845@qq.com>
2019-08-01 10:06:29 +0800

    gin登录页面

---
+++ client/src/components/login_nj.vue
@@ -0,0 +1,25 @@
+<template>
+  <!--小蓝头-->
+  <div class="nav">
+    <!--头部-->
+      <div class="top">
+        <router-link :to="{}"> < </router-link>
+        <h2>密码登录</h2>
+      </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "login_nj"
+    }
+</script>
+
+<style scoped>
+  .top{
+    color: white;
+    height: 39px;
+    background:royalblue;
+    line-height: 39px;
+  }
+</style>
